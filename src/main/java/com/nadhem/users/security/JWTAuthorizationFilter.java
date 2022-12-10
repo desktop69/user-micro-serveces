2712c007ab739567642b4ef67100c3c12d8b6f9c
desktop69 <saadjlassi8@gmail.com>
2022-12-10 13:17:49 +0100

    first commit

--- src/main/java/com/nadhem/users/security/JWTAuthorizationFilter.java
+++ src/main/java/com/nadhem/users/security/JWTAuthorizationFilter.java
@@ -23,46 +23,40 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		
-		
 		response.addHeader("Access-Control-Allow-Origin", "*");
 		response.addHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-		response.addHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, "
-				+ "Origin,Accept, X-Requested-With, Content-Type, "
-				+ "Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-		response.addHeader("Access-Control-Expose-Headers","Authorization,"
-				+ " Access-Control-Allow-Origin,Access-Control-Allow-Credentials ");
+		response.addHeader("Access-Control-Allow-Headers",
+				"Access-Control-Allow-Headers, " + "Origin,Accept, X-Requested-With, Content-Type, "
+						+ "Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+		response.addHeader("Access-Control-Expose-Headers",
+				"Authorization," + " Access-Control-Allow-Origin,Access-Control-Allow-Credentials ");
 
-		if (request.getMethod().equals("OPTIONS"))
-				{
-					response.setStatus(HttpServletResponse.SC_OK);
-		return;
-				}
-		
-		String jwt =request.getHeader("Authorization");
-		
-		
-		if (jwt==null || !jwt.startsWith(SecParams.PREFIX))
-		{
-			filterChain.doFilter(request, response);
-		    return;
+		if (request.getMethod().equals("OPTIONS")) {
+			response.setStatus(HttpServletResponse.SC_OK);
+			return;
 		}
-			
-JWTVerifier verifier = JWT.require(Algorithm.HMAC256(SecParams.SECRET)).build();
-		//enlever le préfixe Bearer du  jwt
-		jwt= jwt.substring(7); // 7 caractères dans "Bearer "
-		
-		DecodedJWT decodedJWT  = verifier.verify(jwt);
+
+		String jwt = request.getHeader("Authorization");
+
+		if (jwt == null || !jwt.startsWith(SecParams.PREFIX)) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		JWTVerifier verifier = JWT.require(Algorithm.HMAC256(SecParams.SECRET)).build();
+		// enlever le préfixe Bearer du jwt
+		jwt = jwt.substring(7); // 7 caractères dans "Bearer "
+
+		DecodedJWT decodedJWT = verifier.verify(jwt);
 		String username = decodedJWT.getSubject();
 		List<String> roles = decodedJWT.getClaims().get("roles").asList(String.class);
-	
-		Collection <GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+
+		Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
 		for (String r : roles)
 			authorities.add(new SimpleGrantedAuthority(r));
-		
-		UsernamePasswordAuthenticationToken user =
-				 new UsernamePasswordAuthenticationToken(username,null,authorities);
-		
+
+		UsernamePasswordAuthenticationToken user = new UsernamePasswordAuthenticationToken(username, null, authorities);
+
 		SecurityContextHolder.getContext().setAuthentication(user);
 		filterChain.doFilter(request, response);
 	}
